7d939cf5d6a9bb2caf9279c7b004040fc6222a4b
lazymg <cbfmark@gmail.com>
2025-03-10 18:05:27 +0900

    Google Login Production

--- src/shared/lib/constant.ts
+++ src/shared/lib/constant.ts
@@ -1,5 +1,5 @@
-const REDIRECT_URL = "http://localhost:5173/google-login";
-
 export const googleLoginUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${
   import.meta.env.VITE_GOOGLE_ID
-}&redirect_uri=${REDIRECT_URL}&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
+}&redirect_uri=${
+  import.meta.env.VITE_REDIRECT_URL
+}&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
