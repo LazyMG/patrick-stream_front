5296b57382d95cf53958394e894a7efef63c92d3
LazyMG <cbfmark@gmail.com>
2025-06-25 12:26:38 +0900

    fix: 모바일 에러 수정 시도

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import { RecoilRoot } from "recoil";
 import GlobalRouterProvider from "./providers/GlobalRouterProvider";
 
 function App() {
-  alert(`alert! ${import.meta.env.VITE_DEV_API_URL}`);
   return (
     <RecoilRoot>
       <GlobalRouterProvider />
