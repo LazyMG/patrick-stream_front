c274f3f194dc2099818cf2617476eedc7bd396d1
LazyMG <cbfmark@gmail.com>
2025-06-25 16:56:43 +0900

    fix: 일시적으로 유튜브 재생 관련 수정 #25

--- src/widgets/YoutubeContainer.tsx
+++ src/widgets/YoutubeContainer.tsx
@@ -74,8 +74,6 @@ function YoutubeContainer() {
     // player?.destroy();
     setPlayer(event.target);
 
-    alert("상태" + event.target.isMute());
-
     // setPlayerInstance(event.target);
     // console.dir(event.target);
     // console.log("play!", player?.getPlayerState());
@@ -97,6 +95,8 @@ function YoutubeContainer() {
   };
 
   const onPlayerPlay: YouTubeProps["onPlay"] = (event) => {
+    alert("상태" + event.target.isMuted());
+
     // if (isMobile && player) {
     //   event.target.unMute();
     // }
