e261c58adab52d0295fc180659de02a5a38d3ae8
LazyMG <cbfmark@gmail.com>
2025-07-01 15:13:27 +0900

    style: 모바일 레이아웃 테스트 #21

--- src/app/GlobalStyle.tsx
+++ src/app/GlobalStyle.tsx
@@ -20,7 +20,7 @@ time, mark, audio, video {
 	font-size: 100%;
 	font: inherit;
 	vertical-align: baseline;
-	overscroll-behavior: none;
+	overscroll-behavior: contain;
 }
 /* HTML5 display-role reset for older browsers */
 article, aside, details, figcaption, figure, 
